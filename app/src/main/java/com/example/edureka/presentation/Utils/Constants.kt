e486512786ff783553d844b341afce4ba01d492b
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-03 10:45:24 +0530

    created constants for list category

---
+++ app/src/main/java/com/example/edureka/presentation/Utils/Constants.kt
@@ -0,0 +1,82 @@
+package com.example.edureka.presentation.Utils
+
+object Constants {
+
+    val numberAsString = listOf("10") + (1..50).map { it.toString() }
+
+
+
+    val categories  = listOf(
+        "General Knowledge",
+
+        "Entertainment: Books",
+        "Entertainment: Films",
+        "Entertainment: Music",
+        "Entertainment: Musicals & Theatres",
+        "Entertainment: Television",
+        "Entertainment: Video Games",
+        " Entertainment: Board Games",
+        "Science & nature",
+        "Science: Computers",
+        "Science: Mathematics",
+        "Mythology",
+        "Sports",
+        "Geography",
+        "History",
+        "Politics",
+        "Art",
+        "Celebrities",
+        "Animals",
+        "Vehicles",
+        "Entertainment: Comics",
+        "Science: Gadgets",
+        "Entertainment: Japanese Anime & Manga",
+        "Entertainment: Cartoon & Animations"
+    )
+
+    val categoriesMap= mapOf(
+        "General Knowledge" to 9,
+        "Entertainment: Books" to 10,
+        "Entertainment: Film" to 11,
+        "Entertainment: Music" to 12,
+        "Entertainment: Musicals & Theatres" to 13,
+        "Entertainment: Television" to 14,
+        "Entertainment :Video Games" to 15,
+        "Entertainment: Board Games" to 16,
+
+
+        "Science & Nature" to 17,
+
+        "Science: Computer" to 18,
+        "Science: Mathematics" to 19,
+        "Mythology" to 20,
+
+        "Sports" to  21,
+
+        "Geography" to 22,
+
+        "History" to 23,
+
+        " Politics" to 24,
+
+        "Art" to 25,
+
+        "Celebrities" to 26,
+
+        "Animals" to 27,
+
+        "Vehicle" to 28,
+
+        "Entertainment: Comics" to 29,
+
+        "Science & Gadgets" to 30,
+
+        "Entertainment: Japanese Anime & Manga" to 31,
+
+        "Entertainment:  Cartoon & Animations" to 32,
+
+        )
+
+    val difficulty = listOf("Easy", "Medium", "Hard")
+    val type = listOf("Multiple Choice","True/False"  )
+}
