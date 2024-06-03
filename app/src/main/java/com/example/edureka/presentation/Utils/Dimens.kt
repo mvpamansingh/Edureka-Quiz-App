bce1b004302a9ce8e9a349f79674a61a3b8e6562
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-03 09:30:43 +0530

    created dimens for the app

---
+++ app/src/main/java/com/example/edureka/presentation/Utils/Dimens.kt
@@ -0,0 +1,31 @@
+package com.example.edureka.presentation.Utils
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+object Dimens {
+    val MediumTextSize = 26.sp
+    val LargeTextSize = 40.sp
+
+    val HomeTopBoxHeight= 150.dp
+
+
+
+
+    val SmallPadding= 15.dp
+    val MediumPadding= 25.dp
+    val LargePadding= 60.dp
+
+
+    val MediumIconSize= 40.dp
+
+    val SmallSpacerHeight= 8.dp
+    val MediumSpacerHeight= 18.dp
+    val MediumBoxHeight= 60.dp
+
+    val LargeCornerRadius= 50.dp
+    val ExtraLargeCornerRadius= 50.dp
+
+
+}
