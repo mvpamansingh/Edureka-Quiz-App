06fa64d6b08b2756c9b96d40d44e71aeceaf6eb7
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-04 11:15:06 +0530

    Implemented HomeScreenViewModel

---
+++ app/src/main/java/com/example/edureka/presentation/home/HomeScreenViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.edureka.presentation.home
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+
+class HomeScreenViewModel: ViewModel(){
+
+
+    private val _homeState= MutableStateFlow(StateHomeScreen())
+    val homeState = _homeState
+    fun onEvent(event: EventHomeScreen){
+
+        when(event)
+        {
+            is EventHomeScreen.SetNumberOfQuizzes -> {
+                _homeState.value = homeState.value.copy(numberOfQuiz = event.numberOfQuizzes)
+            }
+            is EventHomeScreen.SetQuizCategory -> {
+                _homeState.value= homeState.value.copy(category =event.category)
+            }
+            is EventHomeScreen.SetQuizDifficulty -> {
+                _homeState.value= homeState.value.copy(difficulty = event.difficulty)
+            }
+            is EventHomeScreen.SetQuizType -> {
+                _homeState.value= homeState.value.copy(type = event.type)
+            }
+        }
+    }
+}
