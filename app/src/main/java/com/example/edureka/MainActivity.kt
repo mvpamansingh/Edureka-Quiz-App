b86295558010258c5cf157065e808a59e4508cca
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-05 21:39:03 +0530

    some modifications

--- app/src/main/java/com/example/edureka/MainActivity.kt
+++ app/src/main/java/com/example/edureka/MainActivity.kt
@@ -3,29 +3,35 @@ package com.example.edureka
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.colorResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.edureka.navgraph.SetNavGraph
 import com.example.edureka.ui.theme.EdurekaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+       // enableEdgeToEdge()          // remove title bar
         installSplashScreen()
         setContent {
             EdurekaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+               
+                
+                Box(
+                    modifier= Modifier
+                        .fillMaxSize()
+                        .background(color = colorResource(id = R.color.dark_blue_grey)),
+                    contentAlignment = Alignment.Center
+                ) {
+
+                   SetNavGraph()
                 }
             }
         }
@@ -33,18 +39,3 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EdurekaTheme {
-        Greeting("Android")
-    }
-}
