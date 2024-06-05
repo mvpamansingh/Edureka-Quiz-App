716ec993399bf0ffa26a68231e2f86fd9f07cdcb
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-05 21:25:59 +0530

    implemented Navgraph for app

---
+++ app/src/main/java/com/example/edureka/navgraph/NavGraph.kt
@@ -0,0 +1,56 @@
+package com.example.edureka.navgraph
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.edureka.presentation.home.HomeScreen
+import com.example.edureka.presentation.home.HomeScreenViewModel
+import com.example.edureka.presentation.quiz.QuizScreen
+
+
+@Composable
+fun SetNavGraph()
+{
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.HomeScreen.route )
+    {
+        composable(Routes.HomeScreen.route)
+        {
+            val viewModel : HomeScreenViewModel = hiltViewModel()
+            val state by viewModel.homeState.collectAsState()
+            HomeScreen(state = state, event = viewModel::onEvent, navController= navController)
+        }
+
+
+
+        composable(
+            route= Routes.QuizScreen.route,
+            arguments= listOf(
+                navArgument(ARG_KEY_QUIZ_NUMBER){type= NavType.IntType},
+                navArgument(ARG_KEY_QUIZ_CATEGORY){type= NavType.StringType},
+                navArgument(ARG_KEY_QUIZ_DIFFICULTY){type= NavType.StringType},
+                navArgument(ARG_KEY_QUIZ_TYPE){type= NavType.StringType},
+
+                )
+
+        ){
+            val numOfQuizzes = it.arguments?.getInt(ARG_KEY_QUIZ_NUMBER)
+            val category = it.arguments?.getString(ARG_KEY_QUIZ_CATEGORY)
+            val difficulty = it.arguments?.getString(ARG_KEY_QUIZ_DIFFICULTY)
+            val type = it.arguments?.getString(ARG_KEY_QUIZ_TYPE)
+            QuizScreen(numOfQuiz =numOfQuizzes!! , quizCategory =category!! , quizDifficulty = difficulty!!)
+
+        }
+
+
+    }
+
+}
