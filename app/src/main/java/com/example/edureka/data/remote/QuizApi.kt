8b5b29912695ea194e87bd562592dbda324fd2ce
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-07 12:01:15 +0530

    implemented QuizApi interface

---
+++ app/src/main/java/com/example/edureka/data/remote/QuizApi.kt
@@ -0,0 +1,18 @@
+package com.example.edureka.data.remote
+
+import com.example.edureka.data.remote.dto.QuizResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface QuizApi {
+
+
+    @GET
+    suspend fun getQuizzes(
+        @Query("amount") amount:Int,
+        @Query("category") category:Int,
+        @Query("difficulty") difficulty:String,
+        @Query("type") type:String
+    ):QuizResponse
+
+}
