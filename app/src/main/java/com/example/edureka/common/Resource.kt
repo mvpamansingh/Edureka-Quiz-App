b64d4b33e16035ef2f852ac337557930a0762e42
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-07 17:56:31 +0530

    implemented Result file for handling the runtime error , success

---
+++ app/src/main/java/com/example/edureka/common/Resource.kt
@@ -0,0 +1,14 @@
+package com.example.edureka.common
+
+
+
+sealed class Resource<T>(
+    message:String?= null,
+    data:T?=null
+)
+{
+
+    class Loading<T>:Resource<T>()
+    class Success<T>(val data: T?):Resource<T>(data=data)
+    class Error<T>(val message: String?):Resource<T>(message=message)
+}
