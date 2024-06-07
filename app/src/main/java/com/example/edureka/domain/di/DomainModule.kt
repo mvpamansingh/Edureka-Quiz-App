b14bb4f89c3b675fa9c4662604cc63d3e6e52efa
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-07 18:35:47 +0530

    Updated Domain module

--- app/src/main/java/com/example/edureka/domain/di/DomainModule.kt
+++ app/src/main/java/com/example/edureka/domain/di/DomainModule.kt
@@ -3,8 +3,10 @@ package com.example.edureka.domain.di
 import com.example.edureka.domain.repository.QuizRepository
 import com.example.edureka.domain.usecases.GetQuizUseCases
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @InstallIn(SingletonComponent::class)
@@ -14,8 +16,8 @@ object DomainModule {
 
 
 
-
-
+    @Provides
+    @Singleton
     fun provideGetQuizzesUseCases(quizRepository:QuizRepository):GetQuizUseCases{
         return GetQuizUseCases(quizRepository)
     }
