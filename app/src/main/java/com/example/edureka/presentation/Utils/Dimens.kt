6a7e379dc09dec40d74341423d947f57dd3fc00c
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-10 09:01:19 +0530

    updated Dimens file, added more dimension as needed

--- app/src/main/java/com/example/edureka/presentation/Utils/Dimens.kt
+++ app/src/main/java/com/example/edureka/presentation/Utils/Dimens.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.unit.sp
 object Dimens {
     val MediumTextSize = 26.sp
     val LargeTextSize = 40.sp
-
+    val SmallTextSize= 16.sp
     val HomeTopBoxHeight= 150.dp
 
 
@@ -17,7 +17,7 @@ object Dimens {
     val MediumPadding= 25.dp
     val LargePadding= 60.dp
 
-
+    val SmallBorderWidth=2.dp
     val MediumIconSize= 40.dp
 
     val SmallSpacerHeight= 8.dp
