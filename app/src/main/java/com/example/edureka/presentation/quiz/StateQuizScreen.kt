be223f65a46edc0454834efa6a19f6d56fe23d86
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-11 20:58:19 +0530

    Modified Quiz State to handle one Quiz state at a time

--- app/src/main/java/com/example/edureka/presentation/quiz/StateQuizScreen.kt
+++ app/src/main/java/com/example/edureka/presentation/quiz/StateQuizScreen.kt
@@ -6,6 +6,16 @@ data class StateQuizScreen(
 
 
     val isLoading:Boolean = false,
-    val data :List<Quiz> ?= listOf(),
+    val quizState :List<QuizState> = listOf(),
     val error:String= ""
+)
+
+
+
+
+
+data class QuizState(
+    val quiz:Quiz ?= null,
+    val shuffledOptions:List<String> = emptyList(),
+    val selectedOption: Int? = -1
 )
