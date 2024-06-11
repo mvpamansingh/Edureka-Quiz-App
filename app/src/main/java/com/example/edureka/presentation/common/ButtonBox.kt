635f070e3244c4ce909586e001994778de123f76
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-06-11 20:47:08 +0530

    modified the button box composable to be use as Previous, next and submit button

--- app/src/main/java/com/example/edureka/presentation/common/ButtonBox.kt
+++ app/src/main/java/com/example/edureka/presentation/common/ButtonBox.kt
@@ -1,6 +1,7 @@
 package com.example.edureka.presentation.common
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,36 +14,47 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import com.example.edureka.R
 import com.example.edureka.presentation.Utils.Dimens
 
 @Composable
 fun ButtonBox(
     text:String,
-    padding: Dp,
-    onButtonClick: ()->Unit
+    padding: Dp= Dimens.SmallPadding,
+    borderColor: Color= colorResource(id = R.color.blue_grey),
+    containerColor: Color= colorResource(id = R.color.blue_grey),
+    textColor: Color= colorResource(id = R.color.black),
+    fontSize: TextUnit = Dimens.MediumTextSize,
+    fraction:Float= 1f,
+    onButtonClick: ()->Unit,
+
+    //Additonal parameter added according to need
+    
 )
 {
     Box(
         modifier = Modifier
-            .padding(padding)
-            .fillMaxWidth().clickable { onButtonClick() }
+            .padding(padding).border(Dimens.SmallBorderWidth, borderColor, RoundedCornerShape(Dimens.LargeCornerRadius))
+            .fillMaxWidth(fraction)
+            .clickable { onButtonClick() }
             .height(Dimens.MediumBoxHeight)
             .clip(RoundedCornerShape(Dimens.LargeCornerRadius))
             .background(
-                colorResource(id = R.color.blue_grey)
+                containerColor
             ), contentAlignment = Alignment.Center
     ) {
 
 
 
         Text(text = text,
-            fontSize = Dimens.MediumTextSize,
-            style= MaterialTheme.typography.labelMedium.copy(
-                fontWeight = FontWeight.SemiBold,)
+            fontSize = fontSize,
+            style= MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = textColor
             )
     }
 }
